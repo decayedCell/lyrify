5d2c5f6ddbe63e67fa4ae96ca3c44a1d704376ff
Alexandru Cretu <cretu.alx@gmail.com>
2023-03-30 21:11:02 +0300

    Link example from documentation

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>About</div>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 }
